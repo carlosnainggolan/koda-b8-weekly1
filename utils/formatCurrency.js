e5501598bdd54742e2e47260a5c0013f3820a469
Carlos Brandsma Nainggolan <carlosnainggolan@gmail.com>
2026-05-26 17:05:54 +0700

    docs: add JSDoc comments for rupiahFormatter function for clarity

--- utils/formatCurrency.js
+++ utils/formatCurrency.js
@@ -1,3 +1,7 @@
+/**
+ * Formatter untuk mengubah angka menjadi format mata uang Rupiah.
+ * @module utils/formatCurrency
+ */
 export const rupiahFormatter = new Intl.NumberFormat('id-ID', {
   style: 'currency',
   currency: 'IDR',
