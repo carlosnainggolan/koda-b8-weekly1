17f052616d255f46270c3b7e445c8d9dd71d3bde
Carlos Brandsma Nainggolan <carlosnainggolan@gmail.com>
2026-05-26 17:05:09 +0700

    docs: add JSDoc comments for displayCart and displayCartSummary functions for clarity

--- utils/display.js
+++ utils/display.js
@@ -1,5 +1,9 @@
 import {rupiahFormatter} from './formatCurrency.js';
 
+/**
+ * Utility untuk menampilkan isi keranjang belanja dan ringkasan pembelian.
+ * @module utils/display
+ */
 export const displayCart = (cart) => {
   console.log("\n--- Isi Keranjang Saat Ini ---");
   if (cart.length === 0) {
@@ -10,6 +14,11 @@ export const displayCart = (cart) => {
   console.log("-------------------------------\n");
 };
 
+/**
+ * Menampilkan ringkasan isi keranjang belanja dengan format yang rapi.
+ * @param {CartItem[]} cart - Daftar item yang ada di keranjang belanja.
+ * @return {void}
+ */
 export const displayCartSummary = (cart) => {
   cart.forEach((item, index) => {
     console.log(`${index + 1}. ${item.name} (${rupiahFormatter.format(item.price)})`);
