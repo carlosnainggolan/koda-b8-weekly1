02c0ff613871c094e204aacaff540339895e50f7
Carlos Brandsma Nainggolan <carlosnainggolan@gmail.com>
2026-05-26 17:04:27 +0700

    docs: add JSDoc comments for completeCheckout, calculatePayment, and processPayment functions for clarity

--- services/service.js
+++ services/service.js
@@ -1,6 +1,10 @@
 import {rl} from '../actions/order.js';
 import {rupiahFormatter} from '../utils/formatCurrency.js';
 
+/**
+ * Service untuk memproses checkout, menghitung pembayaran, dan menampilkan struk.
+ * @module services/service
+ */
 export const completeCheckout = (cart) => {
   if (cart.length === 0) {
     console.log("Keranjang belanja kosong. Terima kasih!");
@@ -27,9 +31,15 @@ export const completeCheckout = (cart) => {
   processPayment(totalPrice, cart);
 };
 
+/**
+ * Menghitung pembayaran berdasarkan input uang tunai dan total harga, serta menangani validasi input dan kekurangan pembayaran.
+ * @param {string} cashInput - Input uang tunai yang diberikan oleh user.
+ * @param {number} totalPrice - Total harga yang harus dibayar.
+ * @return {Object} Objek yang berisi cashGiven dan changeAmount jika pembayaran berhasil.
+ * @throws {Error} Jika input uang tidak valid atau jika uang yang diberikan kurang dari total harga.
+ */
 export const calculatePayment = (cashInput, totalPrice) => {
   const cashGiven = parseInt(cashInput.replace(/\D/g, ''));
-
   if (isNaN(cashGiven)) {
     throw new Error("Input uang tidak valid. Silakan masukkan angka saja.");
   }
@@ -45,6 +55,12 @@ export const calculatePayment = (cashInput, totalPrice) => {
   };
 };
 
+/**
+ * Memproses pembayaran, menampilkan struk, dan menangani kasus kekurangan pembayaran dengan retry.
+ * @param {number} totalPrice - Total harga yang harus dibayar.
+ * @param {CartItem[]} cart - Daftar item yang ada di keranjang belanja.  
+ * @return {void}
+ */
 const processPayment = (totalPrice, cart) => {
   rl.question("Masukkan jumlah uang tunai yang dibayarkan: Rp. ", (cashInput) => {
    
